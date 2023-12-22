a3579718a59c882669919698e430f0df83e9bfd0
Funfayk082 <82674237+Funfayk082@users.noreply.github.com>
2023-12-22 15:19:54 +0300

    Update PetTests.java

--- src/test/java/PetTests.java
+++ src/test/java/PetTests.java
@@ -13,26 +13,12 @@ import java.util.ArrayList;
 public class PetTests implements Steps {
     @Test(priority = 1)
     public void addNewPet() {
-        ArrayList<String> photoUrls = new ArrayList<>();
-        photoUrls.add("string");
-        ArrayList<TagRequest> tags = new ArrayList<>();
-        tags.add(new TagRequest(0L, "Cat"));
-        CategoryRequest category = new CategoryRequest(0L, "Cat");
-        PetResponse actualPet = PET_API_STEPS.addNewPet(new PetRequest(0L, category, "Pawny", photoUrls, tags, "available" ));
-        PetResponse expectedPet = new PetResponse(9223372036854775807L, category, "Pawny", photoUrls, tags, "available" );
-        Assert.assertEquals(actualPet, expectedPet);
+        PET_API_STEPS.addNewPet(new PetRequest(0L, category, "Pawny", photoUrls, tags, "available" ));
     }
 
     @Test(priority = 2)
     public void updatePet() {
-        ArrayList<String> photoUrls = new ArrayList<>();
-        photoUrls.add("string");
-        ArrayList<TagRequest> tags = new ArrayList<>();
-        tags.add(new TagRequest(0L, "Кот"));
-        CategoryRequest category = new CategoryRequest(0L, "Кот");
-        PetResponse actualPet = PET_API_STEPS.updatePet(new PetRequest(0L, category, "Лапик", photoUrls, tags, "В наличии" ));
-        PetResponse expectedPet = new PetResponse(9223372036854775807L, category, "Лапик", photoUrls, tags, "В наличии" );
-        Assert.assertEquals(actualPet, expectedPet);
+        PET_API_STEPS.updatePet(new PetRequest(0L, category, "Лапик", photoUrls, tags, "В наличии" ));
     }
 
     @Test(priority = 3)
@@ -42,20 +28,11 @@ public class PetTests implements Steps {
 
     @Test(priority = 4)
     public void getPetById() {
-        ArrayList<String> photoUrls = new ArrayList<>();
-        photoUrls.add("string");
-        ArrayList<TagRequest> tags = new ArrayList<>();
-        tags.add(new TagRequest(1L, "string"));
-        CategoryRequest category = new CategoryRequest(1L, "string");
-        PetResponse expectedPet = new PetResponse(1L, category, "doggie", photoUrls, tags, "available" );
-        PetResponse actualPet = PET_API_STEPS.getPetById(1L);
-        Assert.assertEquals(actualPet, expectedPet);
+        PET_API_STEPS.getPetById(1L);
     }
 
     @Test(priority = 5)
     public void deletePetById() {
-        ApiResponse expectedResponse = new ApiResponse(200, "unknown", "1");
-        ApiResponse actualResponse = PET_API_STEPS.deletePetById(1L);
-        Assert.assertEquals(actualResponse, expectedResponse);
+        PET_API_STEPS.deletePetById(1L);
     }
 }
