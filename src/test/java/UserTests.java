99faa6e2abe3a9f041c1e2f44cc9550d60ed053a
Funfayk082 <82674237+Funfayk082@users.noreply.github.com>
2023-12-22 15:13:31 +0300

    Update UserTests.java

--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserTests  implements Steps {
-    @Test(priority = 1)
+    @Test(priority = 10)
     public void getUserByUsername() {
         UserResponse expectedResponse = new UserResponse(9223372036854775807L, "string", "string", "string", "string", "string", "string", 0);
         UserResponse actualResponse = USER_API_STEPS.getUserByUsername("string");
         Assert.assertEquals(actualResponse, expectedResponse);
     }
 
-    @Test(priority = 2)
+    @Test(priority = 11)
     public void updateUser() {
         UserRequest request = new UserRequest(0L, "ff", "string", "string", "string", "string", "string", 0L);
         ApiResponse expectedResponse = new ApiResponse(200, "unknown", "9223372036854775807");
@@ -24,26 +24,26 @@ public class UserTests  implements Steps {
         Assert.assertEquals(actualResponse, expectedResponse);
     }
 
-    @Test(priority = 3)
+    @Test(priority = 12)
     public void deleteUser() {
         ApiResponse expectedResponse = new ApiResponse(200, "unknown", "string");
         ApiResponse actualResponse = USER_API_STEPS.deleteUser("string");
         Assert.assertEquals(actualResponse, expectedResponse);
     }
 
-    @Test(priority = 4)
+    @Test(priority = 13)
     public void login() {
         ApiResponse actualResponse = USER_API_STEPS.login("string", "12345678");
     }
 
-    @Test(priority = 5)
+    @Test(priority = 14)
     public void logout() {
         ApiResponse expectedResponse = new ApiResponse(200, "unknown", "ok");
         ApiResponse actualResponse = USER_API_STEPS.logout();
         Assert.assertEquals(actualResponse, expectedResponse);
     }
 
-    @Test(priority = 6)
+    @Test(priority = 15)
     public void addUserList() {
         List<UserRequest> users = new ArrayList<>();
         users.add(new UserRequest(1L, "Funfayk082", "Андрей", "Зайцев", "Funfayk082@yandex.ru", "abc_123456", "88005553535", 1L));
@@ -52,7 +52,7 @@ public class UserTests  implements Steps {
         Assert.assertEquals(actualResponse, expectedResponse);
     }
 
-    @Test(priority = 7)
+    @Test(priority = 16)
     public void addUser() {
         UserRequest user = new UserRequest(0L, "FF", "A", "Z", "FF@mail.ru", "12345678", "string", 0L);
         ApiResponse expectedResponse = new ApiResponse(200, "unknown", "9223372036854775807");
